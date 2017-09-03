0730ea93a0491163f51167f26c9db89d03431c99
Anna Medvid <anasim669@gmail.com>
2017-09-03 11:08:28 +0300

    replace BrowserRouter with HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware} from 'redux'
 import createSagaMiddleware from 'redux-saga'
@@ -25,9 +25,9 @@ sagaMiddleware.run(rootSaga)
 
 ReactDOM.render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			{routes}
-		</BrowserRouter>
+		</HashRouter>
   	</Provider>,
   document.querySelector('.app')
 )
